a35cfd2fdc199587dbecabd20947423949d50e03
Kelsey Griffin <kjulegriffin@gmail.com>
2020-05-11 16:55:21 -0400

    change allow origin to any

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 3001;
+const port = 8080;
 
 
 const users = require('./users');
@@ -10,7 +10,7 @@ const pantries = require('./pantries');
 app.use(cors()) ;
 app.use(express.json());
 app.use(function(req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
 	next();
