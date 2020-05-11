523fe6ed05358a8a38cf0a7f163c6dc619c1ce66
Kelsey Griffin <kjulegriffin@gmail.com>
2020-05-11 13:13:42 -0400

    change database name to potlucky

--- users.js
+++ users.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
 	user     : 'final',
 	host     : 'localhost',
-	database : 'potlucky1',
+	database : 'potlucky',
 	password : 'root',
 	port     : 5432
 });
