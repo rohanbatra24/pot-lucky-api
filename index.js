54cc69c6be40adbbbeeff047096770eb84d639d9
Kelsey Griffin <kjulegriffin@gmail.com>
2020-05-12 16:29:40 -0400

    set up add to pantry route on backend

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 8080;
+const bodyparser = require("body-parser");
 
 
 const users = require('./users');
@@ -9,14 +10,16 @@ const pantries = require('./pantries');
 
 app.use(cors()) ;
 app.use(express.json());
-app.use(function(req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
-	next();
-});
+app.use(bodyparser.json());
 
-app.get('/', (req, res) => {
+// app.use(function(req, res, next) {
+// 	res.setHeader('Access-Control-Allow-Origin', '*');
+// 	res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+// 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+// 	next();
+// });
+
+app.get('/api/pantries/all', (req, res) => {
 	pantries
 		.getPantries(1)
 		.then((response) => {
@@ -26,17 +29,17 @@ app.get('/', (req, res) => {
 			res.status(500).send(error);
 		});
 });
-
-// app.post('/merchants', (req, res) => {
-// 	merchant_model
-// 		.createMerchant(req.body)
-// 		.then((response) => {
-// 			res.status(200).send(response);
-// 		})
-// 		.catch((error) => {
-// 			res.status(500).send(error);
-// 		});
-// });
+ 
+app.post('/api/pantries/add', (req, res) => {
+	pantries
+		.addItemToPantry(req.body)
+		.then((response) => {
+			res.status(200).send(response);
+		})
+		.catch((error) => {
+			res.status(500).send(error);
+		});
+});
 
 // app.delete('/merchants/:id', (req, res) => {
 // 	merchant_model
